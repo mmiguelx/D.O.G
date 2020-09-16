e23b5718affd5563278caf811e58999cf23b36ab
chokolove <carlosll_24@hotmail.com>
2020-09-16 17:52:44 -0500

    mega on buttons

---
+++ RPS/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    public void btnSpawnPress()
+    {
+        animator.SetBool("spawn", true);
+    }
+    public void btnJumpPress()
+    {
+        animator.SetBool("air", true);
+    }
+    public void btnRunPress()
+    {
+        animator.SetFloat("speed", 1f);
+    }
+}
---
+++ RPS/Assets/Scripts/megamanMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class megamanMovement : MonoBehaviour
+{
+    public Animator animator;
+     void Start()
+    {
+        //animator.SetBool("spawn", false);
+    }
+    void Update()
+    {
+
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("MegaSpawn_loop"))
+        {
+            animator.SetBool("spawn", false);
+        }
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("MegaJump_loop"))
+        {
+            animator.SetBool("air", false);
+        }
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("MegaRun"))
+        {
+            animator.SetFloat("speed", 0f);
+        }
+
+    }
+}
