c70a7693227cc5be162f56cf90dae8aae651bffb
Mario Pernia <57371690+mmiguelx@users.noreply.github.com>
2020-09-16 17:08:15 +0200

    VolumeGeneral

--- RPS/Assets/Scripts/SetVol.cs
+++ RPS/Assets/Scripts/SetVol.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVol : MonoBehaviour
 {
     public AudioMixer mixer;
+    static float value;
+    static bool firstRun = true;
+
+    void Start()
+    {
+        var slider = GetComponent<Slider>();
+        if (value != 1 && firstRun == false)
+        {
+            slider.value = value;
+        }
+        else if (firstRun == true)
+        {
+            slider.value = 1;
+            firstRun = false;
+        }
+    }
+
+    void Update()
+    {
+        var slider = GetComponent<Slider>();
+        if (slider.value != 1)
+        {
+            value = slider.value;
+        }
+    }
 
     public void SetLevel (float sliderValue)
     {
