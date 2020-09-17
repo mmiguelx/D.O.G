d2613416d1bc2b5c7d27b0eee746dc760ba807e2
chokolove <carlosll_24@hotmail.com>
2020-09-17 01:01:31 -0500

    4 enems, more buttons

--- RPS/Assets/Scripts/Units/ButtonBehaviour.cs
+++ RPS/Assets/Scripts/Units/ButtonBehaviour.cs
@@ -4,23 +4,31 @@ public class ButtonBehaviour : MonoBehaviour
 {
     public Animator animator;
     public Animator animator2;
-  
+    public Animator animator3;
+    public Animator animator4;
+
 
     //For the proyect
     public void btnRed()
     {
         animator.SetBool("red", true);
         animator2.SetBool("red", true);
+        animator3.SetBool("red", true);
+        animator4.SetBool("red", true);
     }
     public void btnBlue()
     {
         animator.SetBool("blue", true);
         animator2.SetBool("blue", true);
+        animator3.SetBool("blue", true);
+        animator4.SetBool("blue", true);
     }
     public void btnGreen()
     {
         animator.SetBool("green", true);
         animator2.SetBool("green", true);
+        animator3.SetBool("green", true);
+        animator4.SetBool("green", true);
     }
     public void btnAtt()
     {
@@ -28,10 +36,34 @@ public class ButtonBehaviour : MonoBehaviour
         {
             animator.SetBool("att", false);
             animator2.SetBool("att", false);
+            animator3.SetBool("att", false);
+            animator4.SetBool("att", false);
         } else
         {
             animator.SetBool("att", true);
             animator2.SetBool("att", true);
+            animator3.SetBool("att", true);
+            animator4.SetBool("att", true);
+        }
+    }
+    public void btnChangePosition ()
+    {
+        Flip(animator);
+        Flip(animator2);
+        Flip(animator3);
+        Flip(animator4);
+    }
+    private void Flip(Animator animator)
+    {
+        Vector3 theScale = animator.transform.localScale;
+        if (animator.transform.localScale.x > 0)
+        {
+            theScale.x = -1;
+            animator.transform.localScale = theScale;
+        } else
+        {
+            theScale.x = 1;
+            animator.transform.localScale = theScale;
         }
     }
 
